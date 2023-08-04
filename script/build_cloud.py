37625e797ce88afe2e287bd216d64271e5f0186e
SheikhJebran <sheikhjebran@gmail.com>
2023-08-05 00:02:55 +0530

    pushing the new code
    pushing the new code

--- script/build_cloud.py
+++ script/build_cloud.py
@@ -2,20 +2,34 @@ import requests
 import json
 import sys
 
-url = "https://www.pythonanywhere.com/api/v0/user/PrashantSindhe/consoles/29229942/send_input/"
+from script.pythonAnyWhereConsole import PythonAnyWhereConsole, ConsoleType
 
-payload = json.dumps({
-    "input": "cd vegitables/vegitable/\ngit pull\npython manage.py migrate\ncd ..\ncd ..\n"
-})
-headers = {
-    'Authorization': 'Token d1d33365c22118b0fa2f3ae5905ddd09a6d23e96',
-    'Content-Type': 'application/json',
-    'Cookie': 'csrftoken=B21Nk06BafyezgxCDz4NCrdydzoOmqnxCmLDjNeqpanQavzumryFMXxWUbHqRt9s; sessionid=8ri3fosi6dtepsvvz306d00e62f02085'
-}
 
-response = requests.request("POST", url, headers=headers, data=payload)
+class BuildCloud(PythonAnyWhereConsole):
 
-print(response.text)
+    def execute(self):
+        #self.delete_all_existing_console()
+        console_id = 29719296 #self.create_new_console(ConsoleType.bash)
+        self.pull_latest_changes_on_pythonanywhere(console_id)
 
-if response.status_code != 200:
-    sys.exit(1)
+    def pull_latest_changes_on_pythonanywhere(self, console_id):
+        url = f"https://www.pythonanywhere.com/api/v0/user/PrashantSindhe/consoles/{console_id}/send_input/"
+
+        payload = json.dumps({
+            "input": "cd vegitables/vegitable/\ngit pull\npython manage.py migrate\ncd ..\ncd ..\n"
+        })
+        headers = {
+            'Authorization': 'Token d1d33365c22118b0fa2f3ae5905ddd09a6d23e96',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        if response.status_code != 200:
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    build = BuildCloud()
+    build.execute()
--- script/reload_webapp.py
+++ script/reload_webapp.py
@@ -4,8 +4,7 @@ url = "https://www.pythonanywhere.com/api/v0/user/PrashantSindhe/webapps/Prashan
 
 payload = {}
 headers = {
-  'Authorization': 'Token d1d33365c22118b0fa2f3ae5905ddd09a6d23e96',
-  'Cookie': 'csrftoken=B21Nk06BafyezgxCDz4NCrdydzoOmqnxCmLDjNeqpanQavzumryFMXxWUbHqRt9s; sessionid=8ri3fosi6dtepsvvz306d00e62f02085'
+  'Authorization': 'Token d1d33365c22118b0fa2f3ae5905ddd09a6d23e96'
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
