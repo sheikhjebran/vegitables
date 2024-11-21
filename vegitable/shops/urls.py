a6269c43d6fd86e2787babc35fa727359ec06470
sheikhjebran <sheikhjebran@gmail.com>
2024-11-21 23:58:36 +0530

    Update urls.py

--- vegitable/shops/urls.py
+++ vegitable/shops/urls.py
@@ -17,72 +17,66 @@ urlpatterns = [
     path('home', arrival_view.home, name='home'),
     path('home/prev/<int:page_number>',
          arrival_view.home_prev_page, name="home_prev_page"),
-         arrival_view.home_prev_page, name = "home_prev_page"),
-   path('home/next/<int:page_number>',
+    path('home/next/<int:page_number>',
          arrival_view.home_next_page, name="home_next_page"),
-        arrival_view.home_next_page, name = "home_next_page"),
 
-            # Arrival Entry URL
-            path('add_arrival_entry', arrival_view.add_new_arrival_entry,
+    # Arrival Entry URL
     path('add_arrival_entry', arrival_view.add_new_arrival_entry,
          name='add_new_arrival_entry'),
     path('add_arrival', arrival_view.add_arrival, name='add_arrival'),
     path('add_arrival', arrival_view.add_arrival, name='add_arrival'),
     path('edit_arrival_entry/<int:arrival_id>',
          arrival_view.modify_arrival, name='modify_arrival'),
-         arrival_view.modify_arrival, name = 'modify_arrival'),
 
-         # RestAPI
-         path('get_arrival_goods_item_name', views.get_arrival_goods_item_name,
-         name ='get_arrival_goods_item_name'),
-         path('get_arrival_goods_list', views.get_arrival_goods_list,
-         name ='get_arrival_goods_list'),
-         path('get_arrival_goods_api', views.get_arrival_goods_api,
-         name ='get_arrival_goods_api'),
-         path('get_arrival_duplicate_validation_api', views.get_arrival_duplicate_validation_api,
-         name ='get_arrival_duplicate_validation_api'),
+    # RestAPI
+    path('get_arrival_goods_item_name', views.get_arrival_goods_item_name,
+         name='get_arrival_goods_item_name'),
+    path('get_arrival_goods_list', views.get_arrival_goods_list,
+         name='get_arrival_goods_list'),
+    path('get_arrival_goods_api', views.get_arrival_goods_api,
+         name='get_arrival_goods_api'),
+    path('get_arrival_duplicate_validation_api', views.get_arrival_duplicate_validation_api,
+         name='get_arrival_duplicate_validation_api'),
 
-         # Profile URL
-         path('profile', views.profile, name='profile'),
+    # Profile URL
+    path('profile', views.profile, name='profile'),
 
-         # expenditure_entry Entry URL
-         path('expenditure_entry', expenditure_view.expenditure_entry, name='expenditure_entry'),
-         path('add_expenditure', expenditure_view.add_expenditure_entry,
-         name ='add_expenditure_entry'),
-         path('edit_expense/<int:expenditure_id>',
-         expenditure_view.edit_expense, name ='edit_expense'),
-         path('delete_expense/<int:expenditure_id>',
-         expenditure_view.delete_expense, name ='delete_expense'),
+    # expenditure_entry Entry URL
+    path('expenditure_entry', expenditure_view.expenditure_entry,
+         name='expenditure_entry'),
+    path('add_expenditure', expenditure_view.add_expenditure_entry,
+         name='add_expenditure_entry'),
+    path('edit_expense/<int:expenditure_id>',
+         expenditure_view.edit_expense, name='edit_expense'),
+    path('delete_expense/<int:expenditure_id>',
+         expenditure_view.delete_expense, name='delete_expense'),
 
-         # Sales Entry URL
-         path('sales_bill_entry', views.sales_bill_entry, name='sales_bill_entry'),
-         path('sales_bill/prev/<int:page_number>',
-         views.sales_bill_prev_page, name ="sales_bill_prev_page"),
-         path('sales_bill/next/<int:page_number>',
-         views.sales_bill_next_page, name ="sales_bill_next_page"),
-         path('navigate_to_add_sales_bill_entry', views.navigate_to_add_sales_bill_entry,
-         name ='navigate_to_add_sales_bill_entry'),
-         path('add_sales_bill', views.modify_sales_bill_entry,
-         name ='modify_sales_bill_entry'),
-         path('edit_sales_bill_entry/<int:sales_id>',
-         views.edit_sales_bill_entry, name ='edit_sales_bill_entry'),
+    # Sales Entry URL
+    path('sales_bill_entry', views.sales_bill_entry, name='sales_bill_entry'),
+    path('sales_bill/prev/<int:page_number>',
+         views.sales_bill_prev_page, name="sales_bill_prev_page"),
+    path('sales_bill/next/<int:page_number>',
+         views.sales_bill_next_page, name="sales_bill_next_page"),
+    path('navigate_to_add_sales_bill_entry', views.navigate_to_add_sales_bill_entry,
+         name='navigate_to_add_sales_bill_entry'),
+    path('add_sales_bill', views.modify_sales_bill_entry,
+         name='modify_sales_bill_entry'),
+    path('edit_sales_bill_entry/<int:sales_id>',
+         views.edit_sales_bill_entry, name='edit_sales_bill_entry'),
 
-         # Rest Api for sales entry list
-         path('get_sales_list_for_arrival_item_list', views.get_sales_list_for_arrival_item_list,
-         name ='get_sales_list_for_arrival_item_list'),
+    # Rest Api for sales entry list
+    path('get_sales_list_for_arrival_item_list', views.get_sales_list_for_arrival_item_list,
+         name='get_sales_list_for_arrival_item_list'),
 
-         # patti Entry URL
-         path('patti_list', patti_view.patti_list, name="patti_list"),
-         path('add_new_patti_entry', view=patti_view.add_new_patti_entry,
+    # patti Entry URL
     path('patti_list', patti_view.patti_list, name="patti_list"),
     path('add_new_patti_entry', view=patti_view.add_new_patti_entry,
          name='add_new_patti_entry'),
     path('generate_patti_pdf_bill', view=patti_view.generate_patti_pdf_bill,
-         path('generate_patti_pdf_bill', view=patti_view.generate_patti_pdf_bill,
          name='generate_patti_pdf_bill'),
-         path('edit_patti_entry/<int:patti_id>',
+    path('edit_patti_entry/<int:patti_id>',
          patti_view.edit_patti_entry, name='edit_patti_entry'),
-         patti_view.edit_patti_entry, name='edit_patti_entry'),
+
 
     # Rest Api for the patti
     path('get_all_lorry_number/<str:lorry_date>', view=views.get_lorry_number_for_date,
@@ -92,8 +86,10 @@ urlpatterns = [
 
     # Report
     path('report', views.report, name='report'),
-    path('sales_bill_report', views.sales_bill_report, name='sales_bill_report'),
-    path('report_sales_bill', views.report_sales_bill, name='report_sales_bill'),
+    path('sales_bill_report', views.sales_bill_report,
+         name='sales_bill_report'),
+    path('report_sales_bill', views.report_sales_bill,
+         name='report_sales_bill'),
     path('generate_sales_bill_report', views.generate_sales_bill_report,
          name='generate_sales_bill_report'),
     path('rmc_report', views.rmc_report, name='rmc_report'),
@@ -117,7 +113,8 @@ urlpatterns = [
 
     # Farmer Ledger
     path('farmer_ledger', views.farmer_ledger, name='farmer_ledger'),
-    path('add_farmer_ledger', views.add_farmer_ledger, name='add_farmer_ledger'),
+    path('add_farmer_ledger', views.add_farmer_ledger,
+         name='add_farmer_ledger'),
     path('search_farmer_ledger', views.search_farmer_ledger,
          name='search_farmer_ledger'),
     path('default_farmer_ledger', views.default_farmer_ledger,
@@ -139,7 +136,8 @@ urlpatterns = [
          views.inventory_next_page, name="inventory_next_page"),
 
     # Credit Bill Entry
-    path('credit_bill_entry', views.credit_bill_entry, name="credit_bill_entry"),
+    path('credit_bill_entry', views.credit_bill_entry,
+         name="credit_bill_entry"),
     path('search_credit', views.search_credit, name="search_credit"),
     path('add_credit_bill_amount', views.add_new_credit_bill_entry,
          name="add_new_credit_bill_entry"),
@@ -159,6 +157,5 @@ urlpatterns = [
     # Settings
     path('settings', settings_view.navigate_to_settings, name="settings"),
     path('update_prefix', settings_view.update_prefix, name="update_prefix")
-    path('settings', settings_view.navigate_to_settings, name="settings"),
-    path('update_prefix', settings_view.update_prefix, name="update_prefix")
+
 ]
